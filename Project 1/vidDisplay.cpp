9047900114c24f7aa48e8fcbd5e985bcc5c15b4b
Arpit <123756415+ar-py-grim@users.noreply.github.com>
2026-01-24 11:52:43 -0500

    Add files via upload

--- Project 1/vidDisplay.cpp
+++ Project 1/vidDisplay.cpp
@@ -10,12 +10,23 @@ when pressed 'r' key. User can also save cuurrent frame as image by pressing 's'
 */ 
 
 #include <iostream>
+#include <iomanip>
 #include <opencv2/opencv.hpp>
 #include "filter.h"
 #include "faceDetect.h"
 #include "DA2Network.hpp"
+#include <chrono>
+
 using namespace cv;
 using namespace std;
+
+// returns a double which gives time in seconds for Windows
+double getTime()
+{
+    auto now = chrono::high_resolution_clock::now();   // Get current time
+    auto duration = now.time_since_epoch();                 // Time since epoch
+    return chrono::duration<double>(duration).count(); // Convert to seconds
+}
 
 // all filter modes
 enum DisplayMode
@@ -24,7 +35,8 @@ enum DisplayMode
     gray,
     alt_gray,
     sepia,
-    cblur,
+    cblur1,
+    cblur2,
     xsobel,
     ysobel,
     color_grad,
@@ -37,9 +49,12 @@ enum DisplayMode
 };
 
 // names of filter modes
-const string MODE_NAMES[] ={"color", "gray", "alt_gray", "sepia", "cblur",
+const string MODE_NAMES[] ={"color", "gray", "alt_gray", "sepia", "cblur1", "cblur2",
     "xsobel", "ysobel", "color_grad", "blur_quant", "detect_faces", "emboss", 
     "fog_effect", "face_blur", "face_highlight"};
+
+// number of times to run a filter for timing
+const int Ntimes = 10;
 
 // assigning keys to filter modes
 DisplayMode getDisplayMode(char key)
@@ -50,7 +65,8 @@ DisplayMode getDisplayMode(char key)
     case 'g': return gray;
     case 'h': return alt_gray;
     case 'p': return sepia;
-    case 'b': return cblur;
+    case 'b': return cblur1;
+    case 'u': return cblur2;
     case 'x': return xsobel;
     case 'y': return ysobel;
     case 'm': return color_grad;
@@ -176,46 +192,66 @@ int main(int argc, char* argv[])
 
         case gray:
             cvtColor(frame, opframe, COLOR_BGR2GRAY);
-            imshow("Video", opframe);
             break;
 
         case alt_gray:
             color_to_gray(frame, opframe);
-            imshow("Video", opframe);
             break;
 
         case sepia:
 			sepia_filter(frame, opframe);
-            imshow("Video", opframe);
 			break;
 
-        case cblur:
-            blur5x5_2(frame, opframe);
-            imshow("Video", opframe);
+        case cblur1:
+        {
+            double startTime = getTime();
+            // execute the file on the original image Ntimes
+            for (int i = 0; i < Ntimes; i++)
+            {
+                blur5x5_1(frame, opframe);
+            }
+            // end the timing
+            double endTime = getTime();
+            // compute the time per image
+            double difference = (endTime - startTime) / Ntimes;
+            cout<<"Time per frame(1): "<<fixed<<setprecision(4)<<difference<<"s\n"<< endl;
+        }
+        break;
+
+        case cblur2:
+        {
+            double startTime = getTime();
+            // execute the file on the original image Ntimes
+            for (int i = 0; i < Ntimes; i++)
+            {
+                blur5x5_2(frame, opframe);
+            }
+            // end the timing
+            double endTime = getTime();
+            // compute the time per image
+            double difference = (endTime - startTime) / Ntimes;
+            cout<<"Time per frame(2): "<<fixed<<setprecision(4)<<difference<<"s\n"<< endl;
+        }
             break;
 
         case xsobel:
             sobelX3x3(frame, sobel_x);
             convertScaleAbs(sobel_x, opframe);
-            imshow("Video", opframe);
             break;
 
         case ysobel:
             sobelY3x3(frame, sobel_y);
             convertScaleAbs(sobel_y, opframe);
-            imshow("Video", opframe);
             break;
 
         case color_grad:
             sobelX3x3(frame, sobel_x);
             sobelY3x3(frame, sobel_y);
             magnitude(sobel_x, sobel_y, opframe);
-            imshow("Video", opframe);
             break;
 
         case blur_quant:
             blurQuantize(frame, opframe, 10);
-            imshow("Video", opframe);
             break;
 
         case detect_faces:
@@ -225,18 +261,15 @@ int main(int argc, char* argv[])
             detectFaces(gray_frame, faces);
             frame.copyTo(opframe);
             drawBoxes(opframe, faces);
-            imshow("Video", opframe);
         }
         break;
 
         case emboss:
             embossing(frame, opframe);
-            imshow("Video", opframe);
             break;
 
         case fog_effect:
             fogEffect(frame, opframe, depthNet, 2.0);
-            imshow("Video", opframe);
             break;
 
         case face_blur:
@@ -246,7 +279,6 @@ int main(int argc, char* argv[])
             detectFaces(gray_frame, faces);
             frame.copyTo(opframe);
             faceBlur(opframe, faces);
-            imshow("Video", opframe);
         }
         break;
 
@@ -257,7 +289,6 @@ int main(int argc, char* argv[])
             detectFaces(gray_frame, faces);
             frame.copyTo(opframe);
             faceHiglight(opframe, faces);
-            imshow("Video", opframe);
         }
         break;
 
